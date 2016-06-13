c50d7bafeb2c5a9395ea8e5c2eb14847208e9c05
Heitor Chang <heitorchang@gmail.com>
2016-06-13 15:06:23 -0300

    clean up

--- emacs-keys.el
+++
@@ -1,3 +0,0 @@
-(add-hook 'lisp-mode-hook (lambda () (local-set-key [tab] 'slime-indent-and-complete-symbol) (local-set-key [return] 'newline-and-indent)))
-
-(add-hook 'clojure-mode-hook (lambda () (local-set-key [tab] 'slime-indent-and-complete-symbol) (local-set-key [return] 'newline-and-indent)))
