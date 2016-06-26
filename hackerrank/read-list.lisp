59fab66c3bcaee8d267bba9e40756c69d99a1bee
Heitor Chang <heitorchang@gmail.com>
2016-06-26 18:09:24 -0300

    parsing strings

--- hackerrank/read-list.lisp
+++ hackerrank/read-list.lisp
@@ -1,11 +1,17 @@
+;;; typical usage 
+;; (print-list (my-function (read-list)))
+
 (defun read-list ()
   (let ((n (read *standard-input* nil)))
     (if (null n)
         '()
-        (cons n (read-list)))))
-    
+        (cons n (read-list)))))    
+
 (defun print-list (lst)
   (format t "~{~d~%~}" lst))
 
-;;; typical usage 
-;; (print-list (my-function (read-list)))
+(defun read-space-sep-list ()
+  (read-from-string (concatenate 'string "(" (read-line) ")")))
+
+(defun str-to-list (str)
+  (read-from-string (concatenate 'string "(" str ")")))
