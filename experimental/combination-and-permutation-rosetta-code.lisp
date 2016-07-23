946c36dfe936218b699a7199559e4dc191a8711f
Heitor Chang <heitorchang@gmail.com>
2016-07-23 16:06:38 -0300

    combinations

--- experimental/combination-and-permutation-rosetta-code.lisp
+++ experimental/combination-and-permutation-rosetta-code.lisp
@@ -1,8 +1,8 @@
 ;;; https://rosettacode.org/wiki/Combinations
 
 (defun combinations (lst n)
-  (cond ((null lst) '())
-        ((= n 0) '(()))
+  (cond ((= n 0) '(()))
+        ((null lst) '())
         (t (append (mapcar #'(lambda (y) (cons (car lst) y))
                            (combinations (cdr lst) (- n 1)))
                    (combinations (cdr lst) n)))))
